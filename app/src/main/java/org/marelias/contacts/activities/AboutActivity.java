93cd0ed21a1b07f5e1a100b1cf163e890809abb3
Elyahw <136118735+elyahw@users.noreply.github.com>
2025-06-23 21:45:14 +0100

    Remove bulky list of names from about screen

--- app/src/main/java/org/marelias/contacts/activities/AboutActivity.java
+++ app/src/main/java/org/marelias/contacts/activities/AboutActivity.java
@@ -32,18 +32,5 @@ public class AboutActivity extends AppBaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ((AppCompatTextView) findViewById(R.id.version)).setText(BuildConfig.VERSION_NAME);
-        ListView contributorsList = findViewById(R.id.contributors_list);
-        List<String> names = asList(getResources().getStringArray(R.array.contributor_names));
-        List<String> urls = asList(getResources().getStringArray(R.array.contributor_urls));
-        contributorsList.setAdapter(new ArrayAdapter<String>(this, R.layout.layout_clickable_text_view_item, names) {
-            @NonNull
-            @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                AppCompatTextView textView = (AppCompatTextView) super.getView(position, convertView, parent);
-                textView.setText(names.get(position));
-                textView.setOnClickListener(v -> goToUrl(urls.get(position), AboutActivity.this));
-                return textView;
-            }
-        });
     }
 }
